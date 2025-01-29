91be166a136ed475c16940762163fbeda7ab2843
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2025-01-29 12:03:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,58 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState, useEffect, useMemo } from 'react';
+import UseMemoComponent from './UseMemoComponent';
+import ReactMemoComponent from './ReactMemoComponent';
 
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [newTodo, setNewTodo] = useState('');
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const addTodo = () => {
+    if (newTodo.length > 5) {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  // Calculate expensive calculation using useMemo
+  const expensiveCalculation = useMemo(() => {
+    console.log('Expensive Calculation'); 
+    return todos.length * 1000; 
+  }, [todos]); 
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <button onClick={incrementCounter}>Increment Counter: {counter}</button>
+      <br />
+      <input 
+        type="text" 
+        value={newTodo} 
+        onChange={handleNewTodoChange} 
+        placeholder="Enter new todo (min 6 characters)" 
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+
+      <h2>UseMemo Component</h2>
+      <UseMemoComponent expensiveCalculation={expensiveCalculation} />
+
+      <h2>React.memo Component</h2>
+      <ReactMemoComponent todos={todos} /> 
+    </div>
+  );
+};
+
+export default App;
